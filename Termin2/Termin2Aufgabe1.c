b305c5f1f0cbc150e7df8a9ccbc9398ea7965f83
Michael <michael.hendlich@stud.h-da.de>
2020-11-19 15:21:50 +0100

    Use proper base files

--- Termin2/Termin2Aufgabe1.c
+++ Termin2/Termin2Aufgabe1.c
@@ -4,18 +4,98 @@
 // vom:
 //
 
+#include <stdint.h>
+
+#include "../h/aic.h"
 #include "../h/pio.h"
 #include "../h/pmc.h"
 
-int main(void) {
+typedef enum { IO_SW1, IO_SW2, IO_SW3 } IO_Switches;
+
+typedef enum { IO_Off, IO_On } IO_States;
+
+/*
+ * Liefert den Status der angegebenen Taste.
+ * @param keyNr	Die Taste, dessen Status geprüft werden soll (SW1-SW3)
+ * @return	Gibt 1 zurück, wenn die Taste gedrückt wird, ansonsten 0.
+ * @example	if ( IsKeydown(IO_SW2) ) { ... }
+ */
+inline uint8_t IsKeydown(const IO_Switches keyNr) {
+   StructPIO* piobaseB = PIOB_BASE;
+
+   static const uint8_t keys[] = { KEY1, KEY2, KEY3 };
+
+   if(keyNr > IO_SW3)
+      return 0;
+
+   const uint8_t key = keys[keyNr];
+
+   // Ist die gewünschte Tasten schon unter der Kontrolle der PIO?
+   if(!(piobaseB->PIO_PSR & key)) {
+      piobaseB->PIO_PER = key;
+
+      // Ist die gewünschte Taste als input konfiguriert?
+      if((piobaseB->PIO_OSR & key))
+         piobaseB->PIO_ODR = key;
+   }
+
+   // Aufgabe: Hier wurde das falsche PIO-Register genutzt!
+   // Berichtigen Sie diesen Fehler, damit der Pin-Status ausgelesen wird.
+   // Beachten Sie zudem, ob die Tasten high- oder low-active sind!
+   if((piobaseB->PIO_PSR & key))
+      return 1;
+
+   return 0;
+}
+
+
+/**
+ * Setzt den Zustand der LEDs (an/aus).
+ * @param leds	Binäre representation der gewünschten LED(s)
+ * @param state	Gewünschter Zustand (On/Off)
+ * @example	SetLED( (LED2 | LED5), IO_On)
+ */
+inline void SetLED(uint16_t leds, IO_States state) {
+   StructPIO* piobaseB = PIOB_BASE;
+
+   // Sind die gewünschten LEDs schon unter der Kontrolle der PIO?
+   if(!(piobaseB->PIO_PSR & leds)) {
+      piobaseB->PIO_PER = leds;
+
+      // Sind die gewünschten LEDs als output konfiguriert?
+      if(!(piobaseB->PIO_OSR & leds))
+         piobaseB->PIO_OER = leds;
+   }
+
+   // Aufgabe: Warum gehen die LEDs an, wenn man den Output cleared?
+   if(state == IO_On)
+      piobaseB->PIO_CODR = leds;
+   else
+      piobaseB->PIO_SODR = leds;
+}
+
+void PeripheryInit(void) {
    StructPMC* pmcbase = PMC_BASE; // Basisadresse des PMC
    StructPIO* piobaseB = PIOB_BASE; // Basisadresse der PIOB
-   piobaseB->PIO_PER = ALL_LEDS;
-   piobaseB->PIO_OER = ALL_LEDS;
+
+   // Eventuell benötigte Peripheral-Clocks einschalten (durch PMC)
+   // ...
+
+   // LEDS initialisieren
+   piobaseB->PIO_PER = ALL_LEDS; // Enable PIO for all LEDs
+   piobaseB->PIO_OER = ALL_LEDS; // Set data direction (output) for all LEDs
+
+   // Tasten initialisieren
+   piobaseB->PIO_PER = KEY1 | KEY2; // Enable PIO for the first two switches
+   piobaseB->PIO_ODR = KEY1 | KEY2; // Set data direction (input) for the first two switches
+}
+
+int main(void) {
+   PeripheryInit();
 
    while(1) {
-      piobaseB->PIO_CODR = ALL_LEDS;
-      piobaseB->PIO_SODR = ALL_LEDS;
+      SetLED(ALL_LEDS, IO_On);
+      SetLED(ALL_LEDS, IO_Off);
    }
 
    return 0;
--- Termin2/Termin2Aufgabe4.c
+++ Termin2/Termin2Aufgabe4.c
@@ -12,9 +12,35 @@
 void taste_irq_handler(void) __attribute__((interrupt));
 
 void taste_irq_handler(void) {
+   StructAIC* aicbase = AIC_BASE;
+   StructPIO* piobaseB = PIOB_BASE;
+
+   // ...
+
+   // Aufgabe: Setzen Sie das Register, welches der AIC das Ende des Interrupts meldet, indem
+   // Sie den Interrupt-Status der PIO übergeben!
+   aicbase->AIC_XXXXXR = piobaseB->PIO_XXR;
+}
+
+void InterruptInit(void) {
+   StructAIC* aicbase = AIC_BASE;
+   StructPIO* piobaseB = PIOB_BASE;
+
+   // Aufgabe: Setzen Sie die richtigen Register ein, indem Sie die Kommentare
+   // in den jeweiligen Zeilen als Hinweise nehmen!
+
+   aicbase->AIC_XXXR = (1 << PIOB_ID); // Disable all interrupt handling for PIOB
+   aicbase->AIC_XXXR = (1 << PIOB_ID); // Clear all pending interrupts from PIOB
+
+   aicbase->AIC_XXR[PIOB_ID] = (unsigned int)taste_irq_handler; // Set the irq-handler for interrupts raised by PIOB
+   aicbase->AIC_XXR[PIOB_ID] = AIC_PRIOR; // Set the highest priority for interrupts from PIOB
+
+   piobaseB->PIO_XXR = KEY1 | KEY2; // Enables interrupts in PIOB for the first two switches
 }
 
 int main(void) {
+   InterruptInit();
+
 
    return 0;
 }

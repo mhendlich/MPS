077821c7e12aaa17afe77cfa65cf0e05d46df972
Mauss,Erik <erik.mauss@t-online.de>
2020-12-03 14:05:55 +0100

    Termin3 geupdatet

--- Termin3/Termin3Aufgabe1.c
+++ Termin3/Termin3Aufgabe1.c
@@ -1,74 +1,144 @@
 // Loesung zu Termin3
 // Aufgabe 1
 // von:
-// vom:
-//
+// vom: 
+// 
 
-#include "../h/aic.h"
-#include "../h/pio.h"
+#include <stdint.h>
+
 #include "../h/pmc.h"
 #include "../h/tc.h"
+#include "../h/pio.h"
+#include "../h/aic.h"
 
-void taste_irq_handler(void) __attribute__((interrupt));
-void Timer3_init(void);
+inline uint8_t IsKeydown(const IO_Switches keyNr)
+{
+	StructPIO* piobaseB = PIOB_BASE;	
+	
+	static const uint8_t keys[] = {KEY1, KEY2, KEY3};
+
+	if (keyNr > IO_SW3)
+		return 0;
+
+	const uint8_t key = keys[keyNr];
+
+	// Ist die gewünschte Tasten schon unter der Kontrolle der PIO?
+	if (~piobaseB->PIO_PSR & key)
+	{
+		piobaseB->PIO_PER = key;
+
+		// Ist die gewünschte Taste als input konfiguriert?
+		if (piobaseB->PIO_OSR & key)
+			piobaseB->PIO_ODR = key;
+	}
+
+	// Aufgabe: Hier wurde das falsche PIO-Register genutzt! 
+	// Berichtigen Sie diesen Fehler, damit der Pin-Status ausgelesen wird.
+	// Beachten Sie zudem, ob die Tasten high- oder low-active sind!
+	if ( ( ~piobaseB->PIO_PDSR & key ) ) //wenn low-active pdsr invertieren
+		return 1;
+
+	return 0;
+}
+
+void taste_irq_handler (void) __attribute__ ((interrupt));
+void Timer3_init (void);
 
 
-// Interruptserviceroutine fï¿½r die Tasten SW1 und SW2
-void taste_irq_handler(void) {
-   StructPIO* piobaseB = PIOB_BASE; // Basisadresse PIO B
-   StructAIC* aicbase = AIC_BASE; //__
-
-   // ab hier entsprechend der Aufgabestellung Termin3 Aufgabe 2 ergaenzen
-   // ********************************************************************
-
-
-   aicbase->AIC_EOICR = piobaseB->PIO_ISR; //__
+// Interruptserviceroutine für die Tasten SW1 und SW2
+void taste_irq_handler (void)
+{
+  	StructPIO* piobaseB = PIOB_BASE;		// Basisadresse PIO B
+  	StructAIC* aicbase  = AIC_BASE;			// Basisadresse des Advanced Interrupt Controller
+	StructTC* timerbase3 = TCB3_BASE;
+	
+	if(IsKeydown(IO_SW1)){
+		piobaseB->PIO_PER = (1<<PIOTIOA3);
+		timerbase3->TC_CCR = TC_CLKDIS;
+	}
+	else if (IsKeydown(IO_SW2)){
+		piobaseB->PIO_PDR = (1<<PIOTIOA3);
+		timerbase3->TC_CCR = TC_CLKEN;
+	}
+	
+	aicbase->AIC_EOICR = piobaseB->PIO_ISR;	//__
 }
 
 // Timer3 initialisieren
-// Ein symetrisches Rechtecksignal zur Ansteueren einer Kolbenhubpumpe
+// Ein symetrisches Rechtecksignal zur Ansteueren einer Kolbenhubpumpe 
 // an TIOA3 wird erzeugt
 // Der gegebene Programmrahmen stellt noch nicht die richtige Frequenz ein
-void Timer3_init(void) {
-   StructTC* timerbase3 = TCB3_BASE; // Basisadressse TC Block 1
-   StructPIO* piobaseA = PIOA_BASE; // Basisadresse PIOB
+void Timer3_init( void )
+{
+  StructTC* timerbase3  = TCB3_BASE;	// Basisadressse TC Block 1
+  StructPIO* piobaseA   = PIOA_BASE;	// Basisadresse PIOB
 
-   timerbase3->TC_CCR = TC_CLKDIS; // Disable Clock
+  timerbase3->TC_CCR = TC_CLKDIS;	// Disable Clock
+ 
+  // Initialisieren Timer 3
+  timerbase3->TC_CMR =
+    TC_ACPC_CLEAR_OUTPUT  |    //ACPC    : Register C clear TIOA
+    TC_ACPA_SET_OUTPUT    |    //ACPA    : Register A set TIOA
+    TC_WAVE               |    //WAVE    : Waveform mode
+    TC_CPCTRG             |    //CPCTRG  : Register C compare trigger enable
+    TC_CLKS_MCK8;           //TCCLKS  : MCKI / 8
 
-   // Initialisieren Timer 3
-   timerbase3->TC_CMR = TC_ACPC_CLEAR_OUTPUT | // ACPC    : Register C clear TIOA
-                        TC_ACPA_SET_OUTPUT | // ACPA    : Register A set TIOA
-                        TC_WAVE | // WAVE    : Waveform mode
-                        TC_CPCTRG | // CPCTRG  : Register C compare trigger enable
-                        TC_CLKS_MCK1024; // TCCLKS  : MCKI / 1024
+  // Initialize the counter:
+  timerbase3->TC_RA = 31250;	//Contains the value at which TIOA gets set
+  timerbase3->TC_RC = 62500;	//Contains the value at which TIOA gets cleared
+  
+  //(25MHz/8)/RC = 50Hz
+  //(25MHz/8)/50Hz = RC
+  // RC = 62500
+  // RA = RC/2
 
-   // Initialize the counter:
-   timerbase3->TC_RA = 400; //__
-   timerbase3->TC_RC = 800; //__
-
-   // Start the timer :
-   timerbase3->TC_CCR = TC_CLKEN; //__
-   timerbase3->TC_CCR = TC_SWTRG; //__
-   // Initialisieren vom Ausgang TIOA3 und setzen auf Low-Signal
-   piobaseA->PIO_PER = (1 << PIOTIOA3); //__
-   piobaseA->PIO_OER = (1 << PIOTIOA3); //__
-   piobaseA->PIO_CODR = (1 << PIOTIOA3); //__
+  // Start the timer :
+  timerbase3->TC_CCR = TC_CLKEN ;	//Enables the clock
+  timerbase3->TC_CCR = TC_SWTRG ;	//Sets counter to 0 and starts the clock
+  // Initialisieren vom Ausgang TIOA3 und setzen auf Low-Signal
+  piobaseA->PIO_PER  = (1<<PIOTIOA3) ;	//Enables PIO control on the corresponding pin
+  piobaseA->PIO_OER  = (1<<PIOTIOA3) ;	//Enables output on the corresponding pin
+  piobaseA->PIO_CODR = (1<<PIOTIOA3) ;	//Clears PIO output on the corresponding
 }
 
-int main(void) {
+void InterruptInit(void)
+{
+	StructAIC* aicbase = AIC_BASE;
+	StructPIO* piobaseB = PIOB_BASE;
 
-   StructPMC* pmcbase = PMC_BASE; // Basisadresse des PMC
-   StructPIO* piobaseA = PIOA_BASE; // Basisadresse PIO A
-   StructPIO* piobaseB = PIOB_BASE; // Basisadresse PIO B
+	// Aufgabe: Setzen Sie die richtigen Register ein, indem Sie die Kommentare
+	// in den jeweiligen Zeilen als Hinweise nehmen!
 
-   pmcbase->PMC_PCER = 0x4000; // Peripheral Clocks einschalten fï¿½r PIOB, _____
+	aicbase->AIC_IDCR = (1 << PIOB_ID);	// Disable all interrupt handling for PIOB
+	aicbase->AIC_ICCR = (1 << PIOB_ID);	// Clear all pending interrupts from PIOB
 
-   // ab hier entsprechend der Aufgabestellungen ergaenzen
-   //**************************************************
+	aicbase->AIC_SVR[PIOB_ID] = (unsigned int) taste_irq_handler;	// Set the irq-handler for interrupts raised by PIOB
+	aicbase->AIC_SMR[PIOB_ID] = AIC_PRIOR;				// Set the highest priority for interrupts from PIOB
 
+	aicbase->AIC_IECR = (1 << PIOB_ID);
 
-   while(1) {
-   }
+	piobaseB->PIO_IER = KEY1 | KEY2;	// Enables interrupts in PIOB for the first two switches
+}
 
-   return 0;
+int main(void)
+{
+
+	StructPMC* pmcbase	= PMC_BASE;		// Basisadresse des PMC
+	StructPIO* piobaseA   	= PIOA_BASE;		// Basisadresse PIO A
+	StructPIO* piobaseB   	= PIOB_BASE;		// Basisadresse PIO B
+
+	pmcbase->PMC_PCER	= 0x4200;	// Peripheral Clocks einschalten für PIOB, TC3 
+	
+// ab hier entsprechend der Aufgabestellungen ergaenzen
+//**************************************************
+
+	InterruptInit();
+	Timer3_init();
+	
+	while(1)
+	{
+
+	}
+	
+	return 0;
 }

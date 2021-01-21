295c640ebd937b6bb9314016d56840dead45e934
Michael Hendlich <michael@mycabin.de>
2021-01-21 15:21:49 +0100

    Michis changes

--- Termin5/seriell.c
+++ Termin5/seriell.c
@@ -1,110 +1,114 @@
 /*----------------------------------------------------------------------------
 @ File Name         : seriell.c
-@ Object            : Grundfunktionen der seriellen Schnittstelle 
+@ Object            : Grundfunktionen der seriellen Schnittstelle
 @	int init_ser(); char putch(char); char getch();
 @
 @ Autor 			: M.Pester
 @ Datum				: 04.12.2007
-@ 
+@
 @----------------------------------------------------------------------------*/
 
-#include "../h/pmc.h"
 #include "../h/pio.h"
+#include "../h/pmc.h"
 #include "../h/usart.h"
 
 int init_ser(void);
 char putch(char);
 char getch(void);
-void putstring(char* );
+void putstring(char*);
 
-#define 	DEFAULT_BAUD 38400
-#define 	CLOCK_SPEED 25000000
+#define DEFAULT_BAUD 38400
+#define CLOCK_SPEED 25000000
 // US_BAUD =  (CLOCK_SPEED / (16*(DEFAULT_BAUD))	// 25MHz / ( 16 * 38400) = 40.69  -> 41 -> 0x29
-#define 	US_BAUD 0x29
+#define US_BAUD 0x29
 
 // Initialisiert die serielle Schnittstelle USART0
 
-int init_ser() 
-{
-	StructPIO* piobaseA = PIOA_BASE;	
-	StructPMC* pmcbase  = PMC_BASE;	
-	StructUSART* usartbase0 = USART0;	
-	
-	pmcbase->PMC_PCER = 0x4;	// Clock f�r US0 einschalten
-	piobaseA->PIO_PDR = 0x18000;	// US0 TxD und RxD 
-	usartbase0->US_CR = 0xa0;	// TxD und RxD disable
-	usartbase0->US_BRGR = US_BAUD;	// Baud Rate Generator Register 38,1khz???
-	usartbase0->US_MR = 0x8c0;	// Keine Parit�t, 8 Bit, MCKI
-	usartbase0->US_CR = 0x50;	// TxD und RxD enable
+int init_ser() {
+   StructPIO* piobaseA = PIOA_BASE;
+   StructPMC* pmcbase = PMC_BASE;
+   StructUSART* usartbase0 = USART0;
 
-	return 0;
+   pmcbase->PMC_PCER = 0x4; // Clock f�r US0 einschalten
+   piobaseA->PIO_PDR = 0x18000; // US0 TxD und RxD
+   usartbase0->US_CR = 0xa0; // TxD und RxD disable
+   usartbase0->US_BRGR = US_BAUD; // Baud Rate Generator Register 38,1khz???
+   usartbase0->US_MR = 0x8c0; // Keine Parit�t, 8 Bit, MCKI
+   usartbase0->US_CR = 0x50; // TxD und RxD enable
+
+   return 0;
 }
 
 // Gibt wenn m�glich ein Zeichen auf die serielle Schnittstelle aus
 // und liefert das Zeichen wieder zur�ck
 // wenn eine Ausgabe nicht m�glich war wird eine 0 zur�ck geliefert
 
-char putch(char Zeichen) 
-{
-	StructUSART* usartbase0 = USART0;	
+char putch(char Zeichen) {
+   StructUSART* usartbase0 = USART0;
 
-	if( usartbase0->US_CSR & US_TXRDY )
-	{
-		usartbase0->US_THR = Zeichen;
-	}
-	else
-	{
-		Zeichen = 0;	// wenn keine Ausgabe
-	}
-	return Zeichen;
+   if(usartbase0->US_CSR & US_TXRDY) {
+      usartbase0->US_THR = Zeichen;
+   } else {
+      Zeichen = 0; // wenn keine Ausgabe
+   }
+   return Zeichen;
 }
 
 // Gibt entweder ein empfangenes Zeichen oder eine 0 zur�ck
-char getch(void) 
-{
-	StructUSART* usartbase0 = USART0;	
-	char Zeichen;
+char getch(void) {
+   StructUSART* usartbase0 = USART0;
+   char Zeichen;
 
-	if( usartbase0->US_CSR & US_RXRDY )
-	{
-		Zeichen = usartbase0->US_RHR;
-	}
-	else 
-	{
-		Zeichen = 0;
-	}
+   if(usartbase0->US_CSR & US_RXRDY) {
+      Zeichen = usartbase0->US_RHR;
+   } else {
+      Zeichen = 0;
+   }
 
-	return Zeichen;
+   return Zeichen;
 }
 
 // Gibt einen String ueber putch(char) aus
-void putstring(char* String)
-{
-	int	i = 0;
+void putstring(char* String) {
+   int i = 0;
 
-	while( String[i] != 0)
-	{
-		while(!(putch (String[i]))) {
-
-		}
-		i = i + 1;
-	}
-
+   while(String[i] != 0) {
+      while(!(putch(String[i]))) {
+      }
+      i = i + 1;
+   }
 }
 
-char* signedIntToString(int value) {
-	int mod = 10;
-	char* reverseWord;
-	while (mod < value * 10) {
-		mod = mod * 10;
-	}
+int getIntLength(int value) {
+   int length = !value;
+   while(value) {
+      length++;
+      value /= 10;
+   }
+   return length;
+}
 
-	char buffer[100];
+void signedIntToString(int value, char* targetString) {
+   char* buf = targetString;
+   int length = getIntLength(value);
+   int isNegative = value < 0;
 
-	while (mod >= 10) {
-		int result = (value % mod) / (mod / 10);
-		mod /= 10;
-	}
+   if(isNegative) {
+      *buf = '-';
+      value *= (-1);
+   } else {
+      length--;
+   }
 
+   buf += length;
+   long mod = 10;
 
+   while(mod <= 100000000000) {
+      int result = (value % mod) / (mod / 10);
+      *buf-- = 48 + result;
+      if(buf < targetString || (isNegative && buf <= targetString)) {
+         return;
+      }
+      mod *= 10;
+   }
 }
